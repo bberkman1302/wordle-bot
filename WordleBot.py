f090edee267968f32dc02f654a15513b6d4557a2
Brendan Berkman <bberkman20@riverdale.edu>
2022-05-11 05:27:43 -0400

    init

---
+++ WordleBot.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+
+class WordleBot():
+    def __init__(self, filename):
+        f = open(filename, "r")
+        content = f.read()
+
+        self.words = content.splitlines()
+        self.wordsDict = {}
+        self.charFrequencyMap = self.generateCharFrequencyMap()
+        self.wordScores = []
+
+        self.results = []
+        self.greyLetters = {}
+        self.colors = [] #make this self.colors
+        self.yellowCount = {}
+        
+        self.alpha = "abcdefghijklmnopqrstuvwxyz"
+        self.yellowCount = {}
+        
+
+
+        # print(self.charFrequencyMap)
+
+    def generateCharFrequencyMap(self):
+        charFrequencyMap = {}
+        for word in self.words:
+            for character in word:
+                if character in charFrequencyMap:
+                    charFrequencyMap.update({character: charFrequencyMap[character] + 1})
+                else:
+                    charFrequencyMap[character] = 1
+
+        return charFrequencyMap
+
+    def getCharFrequencyMap(self):
+        return self.charFrequencyMap
+
+    def playWordle(self, result):
+        print("test")
+
+    def genLastYellowCount(self):
+        for char in self.alpha:
+            self.yellowCount[char] = 0
+
+        for item in self.colors:
+            if(item[1] == "yellow"):
+                self.yellowCount.update({item[0]: self.yellowCount[item[0]]+1})
+    
+    def calculateWordScore(self, word):
+        score = 0
+
+        wordFrequencyMap = {}
+        for character in word:
+            if character in wordFrequencyMap:
+                wordFrequencyMap.update({character: wordFrequencyMap[character] + 1})
+            else:
+                wordFrequencyMap[character] = 1
+
+        for character in wordFrequencyMap:
+            score += self.charFrequencyMap[character]
+
+        return score/len(word)
+
+    def generateWordScore(self):
+        self.wordScores.clear()
+        self.removeWords()
+        for word in self.words:
+            self.wordScores.append((word, self.calculateWordScore(word)))
+        return self.wordScores
+
+
+
+    def checkGreys(self, word):
+        for character in word:
+            if(character in self.greyLetters):
+                return(False)
+        return(True)
+
+
+    def checkYellows(self, word):
+        i = 0
+        if(len(self.colors)==0):
+            return(True)
+        self.genLastYellowCount()
+        yellowChecker = {}
+        for char in self.alpha:
+            yellowChecker[char] = 0
+
+        for character in word:
+            if((self.colors[i][1] == "yellow") and (self.colors[i][0] == character)):
+                return(False)
+            elif(self.yellowCount[character]!=0):
+                yellowChecker.update({character: yellowChecker[character] + 1})
+            i += 1
+
+        for char in yellowChecker:
+            if yellowChecker[char] < self.yellowCount[char]:
+                return(False)
+        return(True)
+
+    
+    def checkGreens(self, word):
+        i = 0
+        if(len(self.colors)==0):
+            return(True)
+        for character in word:
+            if( (self.colors[i][1]=="green") and (self.colors[i][0] != character)):
+                return(False)
+            i+=1
+        return(True)
+
+    
+    def updateGreys(self, lst):
+        self.colors = lst
+        greys = []
+        notgrey = []
+        #also updates greys
+        for item in self.colors:
+            if item[1] == "grey":
+                greys.append(item[0])
+            else:
+                notgrey.append(item[0])
+
+        for letter in greys:
+            if letter not in notgrey:
+                self.greyLetters[letter] = 1
+
+
+
+    def removeWords(self):
+        wordLst = []
+        for word in self.words:
+            if(self.checkYellows(word)==False or self.checkGreys(word)==False or self.checkGreens(word)==False):
+                wordLst.append(word)
+        for word in wordLst:
+            self.words.remove(word)
+        
+
+    def findNextWord(self):
+        top = 0
+        for index, wordTuple in enumerate(self.wordScores):
+            if wordTuple[1] > self.wordScores[top][1]:
+                top = index
+        return self.wordScores[top][0]
+
+
+def main():
+    wordle_bot = WordleBot("words.txt")
+    wordle_bot.generateWordScore()
+    print(wordle_bot.findNextWord())
+
+    wordle_bot.updateGreys([('a', 'yellow'), ('r', 'grey'), ('o', 'grey'), ('s', 'grey'), ('e', 'green')])
+    wordle_bot.generateWordScore()
+    print(wordle_bot.findNextWord())
+
+    wordle_bot.updateGreys([('p', 'green'), ('l', 'green'), ('a', 'green'), ('t', 'grey'), ('e', 'green')])
+    wordle_bot.generateWordScore()
+    print(wordle_bot.findNextWord())
+
+    
+    
+
+if __name__ == "__main__":
+    """Run the main function."""
+    main()
---
+++ WordleEngine.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+import random
+from WordleBot import WordleBot
+import time
+
+
+class WordleEngine():
+    def __init__(self, filename):
+        f = open(filename, "r")
+        content = f.read()
+
+        self.words = content.splitlines()
+        self.wordTable = self.createWordTable()
+        f.close()
+
+    def getWords(self):
+        return self.words
+
+    def createWordTable(self):
+        wordTable = {}
+        for word in self.words:
+            wordTable[word] = True
+
+        return wordTable
+
+    def startGame(self):
+        word = random.choice(self.words)
+        print("Welcome to Wordle!")
+        tries = 1
+        userin = self.requestWord()
+        while userin != word and tries < 6:
+            print(self.analyzeGuess(userin, word))
+
+            userin = self.requestWord()
+            tries += 1
+
+        if userin == word:
+            print("Congratulations! You guessed " + userin + " correctly!")
+        else:
+            print("Out of tries! The correct word was " + word)
+    
+    def runBotGame(self):
+        word = random.choice(self.words)
+        wordle_bot = WordleBot("words.txt")
+        tries = 1
+        wordle_bot.generateWordScore()
+        botin = wordle_bot.findNextWord()
+        while botin != word and tries < 6:
+            analyzed = self.analyzeGuess(botin, word)
+            wordle_bot.updateGreys(analyzed)
+            wordle_bot.generateWordScore()
+            botin = wordle_bot.findNextWord()
+            tries +=1
+
+        
+        analyzed = self.analyzeGuess(botin, word)
+        if botin == word:
+            return(tries, 1)
+        else:
+            return(None, 0)
+
+
+    def requestWord(self):
+        userin = input("Please enter a guess: ")
+        if len(userin) != 5:
+            print("Please enter a 5 letter word.")
+            userin = self.requestWord()
+
+        if userin not in self.wordTable:
+            print(userin + " is not a word.")
+            userin = self.requestWord()
+
+        return userin
+
+    def analyzeGuess(self, guess, word):
+        wordFrequencyMap = {}
+        for character in word:
+            if character in wordFrequencyMap:
+                wordFrequencyMap.update({character: wordFrequencyMap[character] + 1})
+            else:
+                wordFrequencyMap[character] = 1
+
+        result = []  # Result is a tuple with character and wordle value (grey, green, yellow)
+        for count, character in enumerate(guess):  # First we check for any characters in the correct place. All
+            # other characters are set to grey tentatively
+            if word[count] == character:
+                result.append((character, "green"))
+                wordFrequencyMap.update({word[count]: wordFrequencyMap[word[count]] - 1})
+            else:
+                result.append((character, "grey"))
+
+        # Now we go back and check for correct characters in the wrong spot using the frequency map
+
+        for count, guessCharacter in enumerate(guess):
+            for wordCharacter in word:
+                if guessCharacter == wordCharacter and result[count][1] != "green":
+                    if wordFrequencyMap[wordCharacter] != 0:
+                        result[count] = (guessCharacter, "yellow")
+                        wordFrequencyMap.update({wordCharacter: wordFrequencyMap[wordCharacter] - 1})
+
+        return result
+
+
+def main():
+    wordle_engine = WordleEngine("words.txt")
+    total = 0
+    for i in range(1000):
+        print(i)
+        result = wordle_engine.runBotGame()[0]
+        if result == None:
+            total += 6
+        else:
+            total += result
+    print(total/1000)
+
+
+
+if __name__ == "__main__":
+    """Run the main function."""
+    main()
